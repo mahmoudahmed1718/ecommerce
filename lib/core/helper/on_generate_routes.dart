19b70d9066f60f925449dbd5f95da45d90a1af91
mahmoud ahmed <mahmoudahmedhsn@gmail>
2025-04-25 16:27:02 +0300

    finsh splash view logic

--- lib/core/helper/on_generate_routes.dart
+++ lib/core/helper/on_generate_routes.dart
@@ -1,3 +1,4 @@
+import 'package:ecommerce/feature/onboarding/presentation/views/on_boarding_view.dart';
 import 'package:ecommerce/feature/splash/presentation/views/splash_view.dart';
 import 'package:flutter/material.dart';
 
@@ -5,6 +6,8 @@ Route<dynamic> onGenerateRoutes(RouteSettings settings) {
   switch (settings.name) {
     case SplashView.routeName:
       return MaterialPageRoute(builder: (context) => const SplashView());
+    case OnBoardingView.routeName:
+      return MaterialPageRoute(builder: (context) => const OnBoardingView());
     default:
       return MaterialPageRoute(builder: (context) => const Scaffold());
   }
--- lib/feature/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/feature/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,9 +1,22 @@
 import 'package:ecommerce/core/utils/app_images.dart';
+import 'package:ecommerce/feature/onboarding/presentation/views/on_boarding_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody> {
+  @override
+  void initState() {
+    excuteNaviation();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -18,5 +31,12 @@ class SplashViewBody extends StatelessWidget {
         SvgPicture.asset(Assets.imagesCircles, fit: BoxFit.fill),
       ],
     );
+  }
+
+  void excuteNaviation() {
+    Future.delayed(const Duration(seconds: 2), () {
+      // ignore: use_build_context_synchronously
+      Navigator.pushReplacementNamed(context, OnBoardingView.routeName);
+    });
   }
 }
