2571622652974ef9577a9912c50a19e1caf4c2eb
mahmoud ahmed <mahmoudahmedhsn@gmail>
2025-04-25 16:01:22 +0300

    create generate route file and splash view

---
+++ lib/feature/splash/presentation/views/splash_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SplashView extends StatelessWidget {
+  static const routeName = 'splash';
+  const SplashView({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
