ff2fa520ebe12cd4d04a0566ae09a786fd09b668
mahmoud ahmed <mahmoudahmedhsn@gmail>
2025-04-30 14:41:19 +0300

    add dots indicator package and create app colors file in utils

---
+++ lib/core/utils/app_colors.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  static const Color primaryColor = Color(0XFF1F5E3B);
+}
--- lib/feature/onboarding/presentation/views/on_boarding_view.dart
+++ lib/feature/onboarding/presentation/views/on_boarding_view.dart
@@ -1,3 +1,5 @@
+import 'package:dots_indicator/dots_indicator.dart';
+import 'package:ecommerce/core/utils/app_colors.dart';
 import 'package:ecommerce/feature/onboarding/presentation/views/widgets/on_boarding_pageview.dart';
 import 'package:flutter/material.dart';
 
@@ -6,6 +8,18 @@ class OnBoardingView extends StatelessWidget {
   static const String routeName = '/onboarding';
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: OnBoardingPageView());
+    return Scaffold(
+      body: Column(
+        children: [
+          Expanded(child: OnBoardingPageView()),
+          DotsIndicator(
+            dotsCount: 2,
+            decorator: DotsDecorator(
+              activeColor: AppColors.primaryColor.withValues(alpha: 0.7),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
