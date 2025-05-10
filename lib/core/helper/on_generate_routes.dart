72bfa29bf74b759eefe9377767b8dd6eec5226b1
mahmoud ahmed <mahmoudahmedhsn@gmail>
2025-05-10 12:46:02 +0300

    Add LoginView and update route generation for login navigation

--- lib/core/helper/on_generate_routes.dart
+++ lib/core/helper/on_generate_routes.dart
@@ -1,3 +1,4 @@
+import 'package:ecommerce/feature/auth/presentation/view/login_view.dart';
 import 'package:ecommerce/feature/onboarding/presentation/views/on_boarding_view.dart';
 import 'package:ecommerce/feature/splash/presentation/views/splash_view.dart';
 import 'package:flutter/material.dart';
@@ -8,6 +9,8 @@ Route<dynamic> onGenerateRoutes(RouteSettings settings) {
       return MaterialPageRoute(builder: (context) => const SplashView());
     case OnBoardingView.routeName:
       return MaterialPageRoute(builder: (context) => const OnBoardingView());
+    case LoginView.routeName:
+      return MaterialPageRoute(builder: (context) => const LoginView());
     default:
       return MaterialPageRoute(builder: (context) => const Scaffold());
   }
