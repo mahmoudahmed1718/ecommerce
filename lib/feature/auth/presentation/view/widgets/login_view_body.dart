8d49d2067b5a01f34eacc1ee118a23f7b6a8fe32
mahmoud ahmed <mahmoudahmedhsn@gmail>
2025-05-10 16:39:28 +0300

    Add social login options for Facebook, Google, and Apple in LoginViewBody

--- lib/feature/auth/presentation/view/widgets/login_view_body.dart
+++ lib/feature/auth/presentation/view/widgets/login_view_body.dart
@@ -1,10 +1,12 @@
 import 'package:ecommerce/constants.dart';
 import 'package:ecommerce/core/utils/app_colors.dart';
+import 'package:ecommerce/core/utils/app_images.dart';
 import 'package:ecommerce/core/utils/app_text_styels.dart';
 import 'package:ecommerce/core/widgets/custom_button.dart';
 import 'package:ecommerce/core/widgets/custom_text_form_field.dart';
 import 'package:ecommerce/feature/auth/presentation/view/widgets/dont_have_an_account_widget.dart';
 import 'package:ecommerce/feature/auth/presentation/view/widgets/or_divder.dart';
+import 'package:ecommerce/feature/auth/presentation/view/widgets/social_login_button.dart';
 import 'package:flutter/material.dart';
 
 class LoginViewBody extends StatelessWidget {
@@ -54,6 +56,24 @@ class LoginViewBody extends StatelessWidget {
             DontHaveAnAccountWidget(),
             const SizedBox(height: 16),
             const OrDivider(),
+            const SizedBox(height: 16),
+            SocialLoginButton(
+              title: 'login with facebook',
+              image: Assets.imagesFacebookIcon,
+              onPressed: () {},
+            ),
+            const SizedBox(height: 16),
+            SocialLoginButton(
+              title: 'login with google',
+              image: Assets.imagesGoogleIcon,
+              onPressed: () {},
+            ),
+            const SizedBox(height: 16),
+            SocialLoginButton(
+              title: 'login with apple',
+              image: Assets.imagesApplIcon,
+              onPressed: () {},
+            ),
           ],
         ),
       ),
