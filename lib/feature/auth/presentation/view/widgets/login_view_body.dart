cbe68d4cc5ecb54e6577fbc237a5276a3fcc11d7
mahmoud ahmed <mahmoudahmedhsn@gmail>
2025-05-10 16:27:00 +0300

    Add OrDivider widget and update LoginViewBody layout

--- lib/feature/auth/presentation/view/widgets/login_view_body.dart
+++ lib/feature/auth/presentation/view/widgets/login_view_body.dart
@@ -4,6 +4,7 @@ import 'package:ecommerce/core/utils/app_text_styels.dart';
 import 'package:ecommerce/core/widgets/custom_button.dart';
 import 'package:ecommerce/core/widgets/custom_text_form_field.dart';
 import 'package:ecommerce/feature/auth/presentation/view/widgets/dont_have_an_account_widget.dart';
+import 'package:ecommerce/feature/auth/presentation/view/widgets/or_divder.dart';
 import 'package:flutter/material.dart';
 
 class LoginViewBody extends StatelessWidget {
@@ -49,8 +50,10 @@ class LoginViewBody extends StatelessWidget {
             ),
 
             CustomButton(onpressed: () {}, text: 'login'),
-            const SizedBox(height: 16),
+            const SizedBox(height: 32),
             DontHaveAnAccountWidget(),
+            const SizedBox(height: 16),
+            const OrDivider(),
           ],
         ),
       ),
