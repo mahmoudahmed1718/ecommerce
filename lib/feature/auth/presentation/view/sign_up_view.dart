45b1ef9aea8cf52e8b17260d817dad511746659a
mahmoud ahmed <mahmoudahmedhsn@gmail>
2025-05-12 07:15:46 +0300

    Wrap SignUpView in BlocProvider to enable state management with SignUpCubit

--- lib/feature/auth/presentation/view/sign_up_view.dart
+++ lib/feature/auth/presentation/view/sign_up_view.dart
@@ -1,15 +1,23 @@
+import 'package:ecommerce/core/services/get_it_service.dart';
 import 'package:ecommerce/core/widgets/custom_appbar.dart';
+
+import 'package:ecommerce/feature/auth/domain/repo/auth_repo.dart';
+import 'package:ecommerce/feature/auth/presentation/manger/cubit/sign_up/sign_up_cubit.dart';
 import 'package:ecommerce/feature/auth/presentation/view/widgets/signup_view_body.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class SignUpView extends StatelessWidget {
   const SignUpView({super.key});
   static const String routeName = '/signUp';
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: buildAppbar(context: context, titel: 'Sign Up'),
-      body: SignupViewBody(),
+    return BlocProvider(
+      create: (context) => SignUpCubit(getIt<AuthRepo>()),
+      child: Scaffold(
+        appBar: buildAppbar(context: context, titel: 'Sign Up'),
+        body: SignupViewBody(),
+      ),
     );
   }
 }
