7da29d8569f41b473822d610a892063688e66bdf
mahmoud ahmed <mahmoudahmedhsn@gmail>
2025-05-12 12:55:59 +0300

    Refactor SignInCubit to include AuthRepo dependency and implement signIn method for user authentication

--- lib/feature/auth/presentation/manger/cubit/cubit/sign_in_cubit.dart
+++ lib/feature/auth/presentation/manger/cubit/cubit/sign_in_cubit.dart
@@ -1,9 +1,27 @@
 import 'package:bloc/bloc.dart';
 import 'package:ecommerce/feature/auth/domain/entities/user_entity.dart';
+import 'package:ecommerce/feature/auth/domain/repo/auth_repo.dart';
 import 'package:meta/meta.dart';
 
 part 'sign_in_state.dart';
 
 class SignInCubit extends Cubit<SignInState> {
-  SignInCubit() : super(SignInInitial());
+  final AuthRepo authRepo;
+  SignInCubit(this.authRepo) : super(SignInInitial());
+
+  Future<void> signIn({required String email, required String password}) async {
+    emit(SignInLoadingState());
+    final result = await authRepo.signInWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
+    result.fold(
+      (faileur) {
+        emit(SignInFailState(faileur.message));
+      },
+      (user) {
+        emit(SignInSuccessState(user));
+      },
+    );
+  }
 }
