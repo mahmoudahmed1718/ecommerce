923d801557f431fdf86aa2b7608a4eac703724c0
mahmoud ahmed <mahmoudahmedhsn@gmail>
2025-05-12 12:43:50 +0300

    Add signInWithEmailAndPassword method with detailed error handling for authentication failures

--- lib/core/services/firebase_auth_services.dart
+++ lib/core/services/firebase_auth_services.dart
@@ -37,4 +37,39 @@ class FirebaseAuthServices {
       throw CustomException(message: 'An unknown error occurred.');
     }
   }
+
+  Future<User?> signInWithEmailAndPassword({
+    required String email,
+    required String password,
+  }) async {
+    try {
+      final credential = await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      return credential.user;
+    } on FirebaseAuthException catch (e) {
+      log(
+        'execption on firebaseAuthServerce.signInWithEmailAndPassword. ${e.toString()}and code is ${e.code}',
+      );
+      if (e.code == 'user-not-found') {
+        throw CustomException(message: 'No user found for that email.');
+      } else if (e.code == 'wrong-password') {
+        throw CustomException(
+          message: 'Wrong password provided for that user.',
+        );
+      } else if (e.code == 'network-request-failed') {
+        throw CustomException(
+          message: 'Please check your internet connection.',
+        );
+      } else {
+        throw CustomException(message: 'An unknown error occurred.');
+      }
+    } catch (e) {
+      log(
+        'execption on firebaseAuthServerce.signInWithEmailAndPassword. ${e.toString()}',
+      );
+      throw CustomException(message: 'An unknown error occurred.');
+    }
+  }
 }
