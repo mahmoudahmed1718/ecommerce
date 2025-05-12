2f1c0b3327d60304f5f1d6ae885e9e568afec705
mahmoud ahmed <mahmoudahmedhsn@gmail>
2025-05-12 13:00:52 +0300

    Refactor SignInView to use BlocProvider for SignInCubit and improve code structure

--- lib/feature/auth/presentation/view/sign_in_view.dart
+++ lib/feature/auth/presentation/view/sign_in_view.dart
@@ -1,35 +1,42 @@
+import 'package:ecommerce/core/services/get_it_service.dart';
 import 'package:ecommerce/core/widgets/custom_appbar.dart';
+import 'package:ecommerce/feature/auth/domain/repo/auth_repo.dart';
+import 'package:ecommerce/feature/auth/presentation/manger/cubit/cubit/sign_in_cubit.dart';
 import 'package:ecommerce/feature/auth/presentation/view/widgets/sign_in_view_body.dart';
 import 'package:ecommerce/generated/locale_keys.g.dart';
 import 'package:flutter/material.dart';
 import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class SignInView extends StatelessWidget {
   const SignInView({super.key});
   static const String routeName = '/login';
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: buildAppbar(
-        context: context,
-        titel: LocaleKeys.Login.tr(),
+    return BlocProvider(
+      create: (context) => SignInCubit(getIt.get<AuthRepo>()),
+      child: Scaffold(
+        appBar: buildAppbar(
+          context: context,
+          titel: LocaleKeys.Login.tr(),
 
-        actions: [
-          IconButton(
-            onPressed: () {
-              final currentLocale = context.locale;
+          actions: [
+            IconButton(
+              onPressed: () {
+                final currentLocale = context.locale;
 
-              if (currentLocale.languageCode == 'ar') {
-                context.setLocale(const Locale('en'));
-              } else {
-                context.setLocale(const Locale('ar'));
-              }
-            },
-            icon: const Icon(Icons.language),
-          ),
-        ],
+                if (currentLocale.languageCode == 'ar') {
+                  context.setLocale(const Locale('en'));
+                } else {
+                  context.setLocale(const Locale('ar'));
+                }
+              },
+              icon: const Icon(Icons.language),
+            ),
+          ],
+        ),
+        body: SignInViewBody(),
       ),
-      body: SignInViewBody(),
     );
   }
 }
