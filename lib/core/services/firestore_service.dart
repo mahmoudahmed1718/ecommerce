c30d2a7dbb546fbd90f19126eb9a2936d8fd5690
mahmoud ahmed <mahmoudahmedhsn@gmail>
2025-05-13 18:58:13 +0300

    Implement FirestoreService to handle data addition in Firestore

--- lib/core/services/firestore_service.dart
+++ lib/core/services/firestore_service.dart
@@ -1,1 +1,12 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:ecommerce/core/services/database_service.dart';
 
+class FirestoreService implements DatabaseService {
+  @override
+  Future<void> addData({
+    required String path,
+    required Map<String, dynamic> data,
+  }) async {
+    await FirebaseFirestore.instance.collection(path).add(data);
+  }
+}
