d17e6101d9e3dafe32e88a7953f84928fc1d59c9
mahmoud ahmed <mahmoudahmedhsn@gmail>
2025-05-14 17:40:59 +0300

    Implement getData method in FirestoreService to retrieve UserEntity from Firestore

--- lib/core/services/firestore_service.dart
+++ lib/core/services/firestore_service.dart
@@ -1,5 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:ecommerce/core/services/database_service.dart';
+import 'package:ecommerce/feature/auth/data/models/user_model.dart';
+import 'package:ecommerce/feature/auth/domain/entities/user_entity.dart';
 
 class FirestoreService implements DatabaseService {
   @override
@@ -11,8 +13,12 @@ class FirestoreService implements DatabaseService {
   }
 
   @override
-  Future<void> getData({required String path, required String documentId}) {
-    // TODO: implement getData
-    throw UnimplementedError();
+  Future<UserEntity> getData({
+    required String path,
+    required String documentId,
+  }) async {
+    var data =
+        await FirebaseFirestore.instance.collection(path).doc(documentId).get();
+    return UserModel.fromJson(data.data() as Map<String, dynamic>);
   }
 }
