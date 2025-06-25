dbca80680b5e68cfc0a5722208e8254effa06749
mahmoud ahmed <mahmoudahmedhsn@gmail>
2025-06-25 17:19:10 +0300

    Add CustomError widget for displaying error messages

---
+++ lib/core/widgets/custom_error.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CustomError extends StatelessWidget {
+  const CustomError({super.key, required this.errorMessage});
+  final String? errorMessage;
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text('$errorMessage'));
+  }
+}
