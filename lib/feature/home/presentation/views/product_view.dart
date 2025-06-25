acc561a36960ed55437ec1382694e71c5069225c
mahmoud ahmed <mahmoudahmedhsn@gmail>
2025-06-25 20:59:32 +0300

    Implement ProductViewBody with state management; fetch products on initialization and enhance UI layout with custom widgets

--- lib/feature/home/presentation/views/product_view.dart
+++ lib/feature/home/presentation/views/product_view.dart
@@ -1,9 +1,21 @@
+import 'package:ecommerce/core/cubits/cubit/products_cubit_cubit.dart';
+import 'package:ecommerce/core/repo/product_repo/product_repo.dart';
+import 'package:ecommerce/core/services/get_it_service.dart';
+import 'package:ecommerce/feature/home/presentation/views/widgets/product_view_body.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class ProductView extends StatelessWidget {
   const ProductView({super.key});
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: const Text('Product View')));
+    return Scaffold(
+      appBar: AppBar(title: const Text('Product View')),
+      body: BlocProvider(
+        create:
+            (context) => ProductsCubit(productRepo: getIt.get<ProductRepo>()),
+        child: ProductViewBody(),
+      ),
+    );
   }
 }
---
+++ lib/feature/home/presentation/views/widgets/product_view_body.dart
@@ -0,0 +1,50 @@
+import 'package:ecommerce/constants.dart';
+import 'package:ecommerce/core/cubits/cubit/products_cubit_cubit.dart';
+import 'package:ecommerce/core/widgets/best_selling_header.dart';
+import 'package:ecommerce/feature/home/presentation/views/widgets/best_selling_view_bloc_builder.dart';
+import 'package:ecommerce/feature/home/presentation/views/widgets/custom_home_app_bar.dart';
+import 'package:ecommerce/feature/home/presentation/views/widgets/custom_search_text_field.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class ProductViewBody extends StatefulWidget {
+  const ProductViewBody({super.key});
+
+  @override
+  State<ProductViewBody> createState() => _ProductViewBodyState();
+}
+
+class _ProductViewBodyState extends State<ProductViewBody> {
+  @override
+  void initState() {
+    super.initState();
+    context.read<ProductsCubit>().fetchProducts();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+      child: CustomScrollView(
+        slivers: [
+          SliverToBoxAdapter(
+            child: Column(
+              children: [
+                SizedBox(height: kVerticalPadding),
+                const CustomHomeAppBar(),
+                const SizedBox(height: kVerticalPadding),
+
+                CustomSearchTextField(),
+                const SizedBox(height: 12),
+
+                BestSellingHeader(),
+                const SizedBox(height: 12),
+              ],
+            ),
+          ),
+          BestSellingGridViewBlocBuilder(),
+        ],
+      ),
+    );
+  }
+}
