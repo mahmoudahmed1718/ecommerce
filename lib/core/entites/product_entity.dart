4aa1b9c15d26d50948b6a0dc4e267a5a07b0ebc1
mahmoud ahmed <mahmoudahmedhsn@gmail>
2025-06-25 14:23:43 +0300

    Add AddProductInputEntity class to handle product input data

---
+++ lib/core/entites/product_entity.dart
@@ -0,0 +1,34 @@
+import 'dart:io';
+
+import 'package:ecommerce/core/entites/review_entity.dart';
+
+class AddProductInputEntity {
+  final String name;
+  final String description;
+  final double price;
+  final String code;
+  final File imageFile;
+  String? imageUrl;
+  final String isFeatured;
+  final int monthExpires;
+  final bool isOrgainic;
+  final int numberOfCalories;
+  final int unitAmount;
+  final num avergeRating = 0;
+  final num rangeCount = 0;
+  final List<ReviewEntity> reviews;
+  AddProductInputEntity({
+    required this.reviews,
+    required this.name,
+    required this.description,
+    required this.price,
+    required this.code,
+    required this.imageFile,
+    this.isOrgainic = false,
+    this.imageUrl,
+    required this.isFeatured,
+    required this.monthExpires,
+    required this.numberOfCalories,
+    required this.unitAmount,
+  });
+}
