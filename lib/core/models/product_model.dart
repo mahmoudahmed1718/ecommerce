e39e3e7db92523ed60b83b82987ed0e946606327
mahmoud ahmed <mahmoudahmedhsn@gmail>
2025-06-25 14:24:55 +0300

    Add AddProductInputModel and ReviewModel classes for product and review handling

---
+++ lib/core/models/product_model.dart
@@ -0,0 +1,59 @@
+import 'package:ecommerce/core/entites/product_entity.dart';
+import 'package:ecommerce/core/models/review_model.dart';
+
+class AddProductInputModel extends AddProductInputEntity {
+  AddProductInputModel({
+    required super.name,
+    required super.description,
+    required super.price,
+    required super.code,
+    required super.imageFile,
+    required super.isFeatured,
+    required super.isOrgainic,
+    super.imageUrl,
+    required super.monthExpires,
+    required super.numberOfCalories,
+    required super.unitAmount,
+    required super.reviews,
+  });
+
+  factory AddProductInputModel.fromEntity(AddProductInputEntity entity) {
+    return AddProductInputModel(
+      name: entity.name,
+      description: entity.description,
+      price: entity.price,
+      code: entity.code,
+      imageFile: entity.imageFile,
+      isFeatured: entity.isFeatured,
+      imageUrl: entity.imageUrl,
+      monthExpires: entity.monthExpires,
+      numberOfCalories: entity.numberOfCalories,
+      unitAmount: entity.unitAmount,
+      isOrgainic: entity.isOrgainic,
+      reviews:
+          entity.reviews
+              .map((review) => ReviewModel.fromEntity(review))
+              .toList(),
+    );
+  }
+  toJson() {
+    return {
+      'name': name,
+      'description': description,
+      'price': price,
+      'code': code,
+      'imageUrl': imageUrl,
+      'isFeatured': isFeatured,
+      'monthExpires': monthExpires,
+      'numberOfCalories': numberOfCalories,
+      'unitAmount': unitAmount,
+      'isOrganic': isOrgainic,
+      'avergeRating': avergeRating,
+      'rangeCount': rangeCount,
+      'reviews':
+          (reviews as List<ReviewModel>)
+              .map((review) => review.toJson())
+              .toList(),
+    };
+  }
+}
---
+++ lib/core/models/review_model.dart
@@ -0,0 +1,38 @@
+import 'package:ecommerce/core/entites/review_entity.dart';
+
+class ReviewModel extends ReviewEntity {
+  ReviewModel({
+    required super.name,
+    required super.description,
+    required super.rating,
+    required super.image,
+    required super.data,
+  });
+  factory ReviewModel.fromEntity(ReviewEntity entity) {
+    return ReviewModel(
+      name: entity.name,
+      description: entity.description,
+      rating: entity.rating,
+      image: entity.image,
+      data: entity.data,
+    );
+  }
+  factory ReviewModel.fromJson(Map<String, dynamic> json) {
+    return ReviewModel(
+      name: json['name'],
+      description: json['description'],
+      rating: json['rating'],
+      image: json['image'],
+      data: json['data'],
+    );
+  }
+  toJson() {
+    return {
+      'name': name,
+      'description': description,
+      'rating': rating,
+      'image': image,
+      'data': data,
+    };
+  }
+}
