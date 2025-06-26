81231840fabc1513a7ede17274abd06e7356dbf4
mahmoud ahmed <mahmoudahmedhsn@gmail>
2025-06-26 15:36:53 +0300

    Refactor CartViewBody: replace CartItem with CartItemList for improved structure and maintainability

---
+++ lib/feature/home/presentation/views/widgets/cart_item_list.dart
@@ -0,0 +1,17 @@
+import 'package:ecommerce/feature/home/presentation/views/widgets/cart_item.dart';
+import 'package:flutter/material.dart';
+
+class CartItemList extends StatelessWidget {
+  const CartItemList({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return SliverList.separated(
+      itemCount: 10,
+      separatorBuilder:
+          (context, index) => Divider(height: 22, color: Color(0XFFF1F1F5)),
+      itemBuilder: (context, index) {
+        return CartItem();
+      },
+    );
+  }
+}
--- lib/feature/home/presentation/views/widgets/cart_view_body.dart
+++ lib/feature/home/presentation/views/widgets/cart_view_body.dart
@@ -1,32 +1,29 @@
 import 'package:ecommerce/constants.dart';
 import 'package:ecommerce/core/widgets/build_app_bar.dart';
 import 'package:ecommerce/feature/home/presentation/views/widgets/cart_header.dart';
-import 'package:ecommerce/feature/home/presentation/views/widgets/cart_item.dart';
+import 'package:ecommerce/feature/home/presentation/views/widgets/cart_item_list.dart';
 import 'package:flutter/material.dart';
 
 class CartViewBody extends StatelessWidget {
   const CartViewBody({super.key});
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 16.0),
-      child: CustomScrollView(
-        slivers: [
-          SliverToBoxAdapter(
-            child: Column(
-              children: [
-                SizedBox(height: kVerticalPadding),
-                buildAppBar(context, title: 'Products', isNotification: false),
-                const SizedBox(height: kVerticalPadding),
-                CartHeader(),
+    return CustomScrollView(
+      slivers: [
+        SliverToBoxAdapter(
+          child: Column(
+            children: [
+              SizedBox(height: kVerticalPadding),
+              buildAppBar(context, title: 'Products', isNotification: false),
+              const SizedBox(height: kVerticalPadding),
+              CartHeader(),
 
-                const SizedBox(height: 12),
-                CartItem(),
-              ],
-            ),
+              const SizedBox(height: 12),
+            ],
           ),
-        ],
-      ),
+        ),
+        CartItemList(),
+      ],
     );
   }
 }
