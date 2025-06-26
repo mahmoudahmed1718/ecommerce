b861173bb01130b2597eadcd99322033474da124
mahmoud ahmed <mahmoudahmedhsn@gmail>
2025-06-26 16:37:53 +0300

    fix: update CartItemEntity to use ProductEntity and enhance item count management

--- lib/feature/home/domain/entites/cart_item_entity.dart
+++ lib/feature/home/domain/entites/cart_item_entity.dart
@@ -1,8 +1,23 @@
-import 'package:ecommerce/core/repo/product_repo/product_repo.dart';
+import 'package:ecommerce/core/entites/product_entity.dart';
 
 class CartItemEntity {
-  final ProductRepo product;
+  final ProductEntity product;
   int countItem;
 
   CartItemEntity({required this.product, this.countItem = 0});
+  num calculateTotalPrice() {
+    return product.price * countItem;
+  }
+
+  num calculateTotalWeight() {
+    return product.unitAmount * countItem;
+  }
+
+  increaseCount() {
+    countItem++;
+  }
+
+  decreaseCount() {
+    countItem--;
+  }
 }
