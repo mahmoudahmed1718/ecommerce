d46740cf919619732e236831623169ae90ed999c
mahmoud ahmed <mahmoudahmedhsn@gmail>
2025-06-27 15:37:56 +0300

    feat: enhance CartCubit with product management methods and update CartEntity for item existence checks

--- lib/feature/home/domain/entites/cart_entity.dart
+++ lib/feature/home/domain/entites/cart_entity.dart
@@ -1,3 +1,4 @@
+import 'package:ecommerce/core/entites/product_entity.dart';
 import 'package:ecommerce/feature/home/domain/entites/cart_item_entity.dart';
 
 class CartEntity {
@@ -7,6 +8,24 @@ class CartEntity {
 
   addCartItem(CartItemEntity cartItem) {
     carts.add(cartItem);
+  }
+
+  bool isCartItemExist(ProductEntity product) {
+    for (var cartItem in carts) {
+      if (cartItem.product == product) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  CartItemEntity getCartItem(ProductEntity product) {
+    for (var cartItem in carts) {
+      if (cartItem.product == product) {
+        return cartItem;
+      }
+    }
+    return CartItemEntity(product: product, countItem: 1);
   }
 
   removeCartItem(CartItemEntity cartItem) {
--- lib/feature/home/presentation/manger/cart_cubit/cart_cubit.dart
+++ lib/feature/home/presentation/manger/cart_cubit/cart_cubit.dart
@@ -1,4 +1,5 @@
 import 'package:bloc/bloc.dart';
+import 'package:ecommerce/core/entites/product_entity.dart';
 import 'package:ecommerce/feature/home/domain/entites/cart_entity.dart';
 import 'package:ecommerce/feature/home/domain/entites/cart_item_entity.dart';
 import 'package:meta/meta.dart';
@@ -8,8 +9,14 @@ part 'cart_state.dart';
 class CartCubit extends Cubit<CartState> {
   CartEntity cartEntity = CartEntity(carts: []);
   CartCubit() : super(CartInitial());
-  void addCartItem(CartItemEntity cartItem) {
-    cartEntity.addCartItem(cartItem);
+  void addProduct(ProductEntity product) {
+    bool isExist = cartEntity.isCartItemExist(product);
+    var cartItem = cartEntity.getCartItem(product);
+    if (isExist) {
+      cartItem.increaseCount();
+    } else {
+      cartEntity.addCartItem(cartItem);
+    }
     emit(CartItemAdd());
   }
 
