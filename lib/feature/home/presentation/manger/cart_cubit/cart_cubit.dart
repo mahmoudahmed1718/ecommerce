d9c84f5bbd23c50743fd470f09acdc0a87e33f62
mahmoud ahmed <mahmoudahmedhsn@gmail>
2025-06-27 15:08:50 +0300

    feat: implement addCartItem method in CartCubit to manage cart item addition

--- lib/feature/home/presentation/manger/cart_cubit/cart_cubit.dart
+++ lib/feature/home/presentation/manger/cart_cubit/cart_cubit.dart
@@ -1,8 +1,15 @@
 import 'package:bloc/bloc.dart';
+import 'package:ecommerce/feature/home/domain/entites/cart_entity.dart';
+import 'package:ecommerce/feature/home/domain/entites/cart_item_entity.dart';
 import 'package:meta/meta.dart';
 
 part 'cart_state.dart';
 
 class CartCubit extends Cubit<CartState> {
+  CartEntity cartEntity = CartEntity(carts: []);
   CartCubit() : super(CartInitial());
+  void addCartItem(CartItemEntity cartItem) {
+    cartEntity.addCartItem(cartItem);
+    emit(CartItemAdd());
+  }
 }
