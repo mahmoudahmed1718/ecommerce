b45a4fe1a1a0393f2c17a9ee6baa165cd6b651e0
mahmoud ahmed <mahmoudahmedhsn@gmail>
2025-06-27 15:12:53 +0300

    feat: refactor MainView to include BlocProvider for CartCubit and improve localization handling

--- lib/feature/home/presentation/views/main_view.dart
+++ lib/feature/home/presentation/views/main_view.dart
@@ -1,11 +1,13 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:ecommerce/core/services/firebase_auth_services.dart';
+import 'package:ecommerce/feature/home/presentation/manger/cart_cubit/cart_cubit.dart';
 import 'package:ecommerce/feature/home/presentation/views/cart_view.dart';
 import 'package:ecommerce/feature/home/presentation/views/product_view.dart';
 import 'package:ecommerce/feature/home/presentation/views/widgets/custom_bottom_navigation_bar.dart';
 import 'package:ecommerce/feature/home/presentation/views/home_view.dart';
 import 'package:ecommerce/feature/home/presentation/views/widgets/main_view_body.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class MainView extends StatefulWidget {
   const MainView({super.key});
@@ -19,40 +21,43 @@ class _MainViewState extends State<MainView> {
   int currentViewIndex = 0;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      bottomNavigationBar: CustomBottomNavigationBar(
-        onItemTapped: (index) {
-          currentViewIndex = index;
+    return BlocProvider(
+      create: (context) => CartCubit(),
+      child: Scaffold(
+        bottomNavigationBar: CustomBottomNavigationBar(
+          onItemTapped: (index) {
+            currentViewIndex = index;
 
-          setState(() {});
-        },
-      ),
-      appBar: AppBar(
-        actions: [
-          IconButton(
+            setState(() {});
+          },
+        ),
+        appBar: AppBar(
+          actions: [
+            IconButton(
+              onPressed: () {
+                context.setLocale(
+                  context.locale.languageCode == 'en'
+                      ? const Locale('ar')
+                      : const Locale('en'),
+                );
+              },
+              icon: Icon(Icons.language),
+            ),
+          ],
+          title: TextButton(
             onPressed: () {
-              context.setLocale(
-                context.locale.languageCode == 'en'
-                    ? const Locale('ar')
-                    : const Locale('en'),
+              FirebaseAuthServices().signOut();
+              Navigator.pushNamedAndRemoveUntil(
+                context,
+                '/login', // Replace with your login route name
+                (route) => false,
               );
             },
-            icon: Icon(Icons.language),
+            child: Text('Logout', style: TextStyle(color: Colors.red)),
           ),
-        ],
-        title: TextButton(
-          onPressed: () {
-            FirebaseAuthServices().signOut();
-            Navigator.pushNamedAndRemoveUntil(
-              context,
-              '/login', // Replace with your login route name
-              (route) => false,
-            );
-          },
-          child: Text('Logout', style: TextStyle(color: Colors.red)),
         ),
+        body: SafeArea(child: MainViewBody(currentViewIndex: currentViewIndex)),
       ),
-      body: SafeArea(child: MainViewBody(currentViewIndex: currentViewIndex)),
     );
   }
 
