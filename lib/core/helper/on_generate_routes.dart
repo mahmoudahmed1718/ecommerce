562a8a604384c6ff53479479365b19571afaf36f
mahmoud ahmed <mahmoudahmedhsn@gmail>
2025-07-02 09:19:47 +0300

    feat: add CheckoutView and update route generation for checkout navigation

--- lib/core/helper/on_generate_routes.dart
+++ lib/core/helper/on_generate_routes.dart
@@ -1,6 +1,7 @@
 import 'package:ecommerce/feature/auth/presentation/view/sign_in_view.dart';
 import 'package:ecommerce/feature/auth/presentation/view/sign_up_view.dart';
 import 'package:ecommerce/feature/best_selling_fruits/presentation/views/best_selling_fruits_view.dart';
+import 'package:ecommerce/feature/checkout/presentation/view/checkout_view.dart';
 import 'package:ecommerce/feature/home/presentation/views/main_view.dart';
 import 'package:ecommerce/feature/onboarding/presentation/views/on_boarding_view.dart';
 import 'package:ecommerce/feature/splash/presentation/views/splash_view.dart';
@@ -18,6 +19,9 @@ Route<dynamic> onGenerateRoutes(RouteSettings settings) {
       return MaterialPageRoute(builder: (context) => const SignUpView());
     case MainView.routeName:
       return MaterialPageRoute(builder: (context) => const MainView());
+
+    case CheckoutView.routeName:
+      return MaterialPageRoute(builder: (context) => const CheckoutView());
     case BestSellingFruitsView.routeName:
       return MaterialPageRoute(
         builder: (context) => const BestSellingFruitsView(),
