2ce2f1e94abf09231a1685e622b8ef684b6146fa
mahmoud ahmed <mahmoudahmedhsn@gmail>
2025-07-20 15:29:06 +0300

    feat: implement PaymentSection widget to display order summary with spacing

--- lib/feature/checkout/presentation/views/widgets/payment_section.dart
+++ lib/feature/checkout/presentation/views/widgets/payment_section.dart
@@ -1,9 +1,10 @@
+import 'package:ecommerce/feature/checkout/presentation/views/widgets/order_summry_widget.dart';
 import 'package:flutter/material.dart';
 
 class PaymentSection extends StatelessWidget {
   const PaymentSection({super.key});
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(children: [SizedBox(height: 20), OrderSummryWidget()]);
   }
 }
