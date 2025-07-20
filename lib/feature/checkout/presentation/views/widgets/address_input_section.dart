ec8ad8c153c071cbc7943940060e644467f6a8be
mahmoud ahmed <mahmoudahmedhsn@gmail>
2025-07-20 15:04:30 +0300

    feat: implement AddressInputSection with input fields for user details

--- lib/feature/checkout/presentation/views/widgets/address_input_section.dart
+++ lib/feature/checkout/presentation/views/widgets/address_input_section.dart
@@ -1,9 +1,46 @@
+import 'package:ecommerce/feature/home/presentation/views/widgets/custom_text_form_field.dart';
 import 'package:flutter/material.dart';
 
 class AddressInputSection extends StatelessWidget {
   const AddressInputSection({super.key});
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          const SizedBox(height: 24),
+          CustomTextFormField(
+            hintText: 'Name',
+            textInputType: TextInputType.name,
+          ),
+          SizedBox(height: 16),
+          CustomTextFormField(
+            hintText: 'Email',
+            textInputType: TextInputType.emailAddress,
+          ),
+          SizedBox(height: 16),
+          CustomTextFormField(
+            hintText: 'Address',
+            textInputType: TextInputType.streetAddress,
+          ),
+          SizedBox(height: 16),
+          CustomTextFormField(
+            hintText: 'city',
+            textInputType: TextInputType.text,
+          ),
+          SizedBox(height: 16),
+          CustomTextFormField(
+            hintText: 'Your ID',
+            textInputType: TextInputType.text,
+          ),
+          SizedBox(height: 16),
+          CustomTextFormField(
+            hintText: 'Phone Number',
+            textInputType: TextInputType.number,
+          ),
+          SizedBox(height: 16),
+        ],
+      ),
+    );
   }
 }
