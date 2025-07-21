12d431e1bdc04f284710f3185f7264f165ce11b9
mahmoud ahmed <mahmoudahmedhsn@gmail>
2025-07-21 18:53:28 +0300

    add factory method to create ItemList from CartItemEntity list

--- lib/feature/checkout/domain/entites/paypal_payment_entity/item_list.dart
+++ lib/feature/checkout/domain/entites/paypal_payment_entity/item_list.dart
@@ -1,9 +1,16 @@
+import 'package:ecommerce/feature/home/domain/entites/cart_item_entity.dart';
+
 import 'item.dart';
 
 class ItemList {
   List<Item>? items;
 
   ItemList({this.items});
+  factory ItemList.fromEntities(List<CartItemEntity> itemEntities) {
+    return ItemList(
+      items: itemEntities.map((item) => Item.fromEntity(item: item)).toList(),
+    );
+  }
 
   Map<String, dynamic> toJson() => {
     'items': items?.map((e) => e.toJson()).toList(),
