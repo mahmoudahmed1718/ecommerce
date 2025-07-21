9fe306094a141077bb90323ac991569351b6246a
mahmoud ahmed <mahmoudahmedhsn@gmail>
2025-07-21 18:57:51 +0300

    refactor: add fromEntities factory method to PaypalPaymentEntity for improved instantiation

--- lib/feature/checkout/domain/entites/paypal_payment_entity/paypal_payment_entity.dart
+++ lib/feature/checkout/domain/entites/paypal_payment_entity/paypal_payment_entity.dart
@@ -8,9 +8,13 @@ class PaypalPaymentEntity {
 
   PaypalPaymentEntity({this.amount, this.description, this.itemList});
 
-  Map<String, dynamic> toJson() => {
-    'amount': amount?.toJson(),
-    'description': description,
-    'item_list': itemList?.toJson(),
-  };
+  factory PaypalPaymentEntity.fromEntities({
+    required PaypalPaymentEntity entity,
+  }) {
+    return PaypalPaymentEntity(
+      amount: entity.amount,
+      description: entity.description,
+      itemList: entity.itemList,
+    );
+  }
 }
