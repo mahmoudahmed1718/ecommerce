fa38e514070f701c41cc39fc2c50f07367cfdbe0
mahmoud ahmed <mahmoudahmedhsn@gmail>
2025-07-21 18:43:50 +0300

    refactor: clean up formatting and remove unused factory methods in Amount and PaypalPaymentEntity classes

--- lib/feature/checkout/domain/entites/paypal_payment_entity/amount.dart
+++ lib/feature/checkout/domain/entites/paypal_payment_entity/amount.dart
@@ -1,23 +1,15 @@
 import 'details.dart';
 
 class Amount {
-	String? total;
-	String? currency;
-	Details? details;
+  String? total;
+  String? currency;
+  Details? details;
 
-	Amount({this.total, this.currency, this.details});
+  Amount({this.total, this.currency, this.details});
 
-	factory Amount.fromJson(Map<String, dynamic> json) => Amount(
-				total: json['total'] as String?,
-				currency: json['currency'] as String?,
-				details: json['details'] == null
-						? null
-						: Details.fromJson(json['details'] as Map<String, dynamic>),
-			);
-
-	Map<String, dynamic> toJson() => {
-				'total': total,
-				'currency': currency,
-				'details': details?.toJson(),
-			};
+  Map<String, dynamic> toJson() => {
+    'total': total,
+    'currency': currency,
+    'details': details?.toJson(),
+  };
 }
