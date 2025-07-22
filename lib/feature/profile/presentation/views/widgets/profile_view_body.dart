b65920c06bb82d130427cc5e2cebc2a2c8fa60b0
mahmoud ahmed <mahmoudahmedhsn@gmail>
2025-07-22 16:09:19 +0300

    refactor ProfileViewBody to StatefulWidget and add notification and theme toggle functionality

--- lib/feature/profile/presentation/views/widgets/profile_view_body.dart
+++ lib/feature/profile/presentation/views/widgets/profile_view_body.dart
@@ -1,11 +1,21 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:ecommerce/feature/profile/presentation/views/edit_data_profile_view.dart';
 import 'package:ecommerce/feature/profile/presentation/views/widgets/data_profile.dart';
 import 'package:ecommerce/feature/profile/presentation/views/widgets/general_word_widget.dart';
 import 'package:ecommerce/feature/profile/presentation/views/widgets/profile_item.dart';
 import 'package:flutter/material.dart';
 
-class ProfileViewBody extends StatelessWidget {
+class ProfileViewBody extends StatefulWidget {
   const ProfileViewBody({super.key});
+
+  @override
+  State<ProfileViewBody> createState() => _ProfileViewBodyState();
+}
+
+class _ProfileViewBodyState extends State<ProfileViewBody> {
+  bool isSwitchedNotification = false;
+  bool isSwitchedTheme = false;
+
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -29,6 +39,47 @@ class ProfileViewBody extends StatelessWidget {
               color: Colors.grey,
             ),
           ),
+          const SizedBox(height: 16),
+          ProfileItem(
+            onTap: () {},
+            icon: Icons.favorite_outline,
+            text: 'My Favorite Products',
+            childIcon: Icon(
+              Icons.arrow_forward_ios_outlined,
+              size: 16,
+              color: Colors.grey,
+            ),
+          ),
+          const SizedBox(height: 16),
+
+          ProfileItem(
+            onTap: () {},
+            icon: Icons.notifications_none_outlined,
+            text: 'Notifications',
+            childIcon: Switch(
+              value: isSwitchedNotification,
+              onChanged: (value) {
+                setState(() {
+                  isSwitchedNotification = value;
+                });
+              },
+            ),
+          ),
+          const SizedBox(height: 16),
+          ProfileItem(
+            onTap: () {},
+            icon: Icons.notifications_none_outlined,
+            text: 'Dark Mode',
+            childIcon: Switch(
+              value: isSwitchedTheme,
+              onChanged: (value) {
+                setState(() {
+                  isSwitchedTheme = value;
+                });
+              },
+            ),
+          ),
+          const SizedBox(height: 16),
         ],
       ),
     );
